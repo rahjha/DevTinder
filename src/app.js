3ec2126a6db4dccb0a57614dbb82000ff2ccd6c4
rahjha <rahuljha.kumar25@gmail.com>
2025-01-11 22:11:50 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,36 +1,17 @@
 const express = require("express");
-
 const app = express();
-const {adminAuth,userAuth} = require("../middleware/adminAuth");
 
-//app.use("/user",rh1, rh2, rh3, rh4, rh5)
-//app.use("/user",[rh1, rh2, rh3, rh4, rh5])
-//app.use("/user",rh1, [rh2, rh3, rh4], rh5)
+app.use("/user",(req, res)=>{
+    
+    try{
+        throw new Error("Customer error");
+    }catch{
+        next();
+    }
+});
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData",(req, res)=>{
-    console.log("/admin/getAllData is called");
-    res.send("All data is fetched");
-})
-
-app.get("/admin/deleteData",(req, res)=>{
-    console.log("/admin/deleteData is called");
-    res.send("Data is deleted");
-})
-
-app.use("/user/data",userAuth, (req, res, next)=>{
-    console.log("Handling the route user!!");
-    //res.send("1st Response");
-    next();
-},(req, res)=>{
-    console.log("Handling the route user 2!!");
-    res.send("2nd Response");
-})
-
-app.use("/user/login",(req, res)=>{
-    console.log("User login is called");
-    res.send("User is logged in")
+app.use("/user",(err, req, res, next)=>{
+    res.status(500).send("something went wrong");
 })
 
 app.listen(7777,()=>{
